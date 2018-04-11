e04b0571914bc8ae470ccb075d6a6b092f51fdad
Marcus Höglund <marcus.hoglund@collectorbank.se>
2018-04-11 16:19:32 +0200

    added initial files

---
+++ MackHog.Cache.Core/CacheExtender.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace MackHog.Cache.Core
+{
+    public static class CacheExtender
+    {
+        public static void AddCache(this IServiceCollection services)
+        {
+            services.AddMemoryCache();
+            services.AddTransient<ICache, Cache>();
+        }
+    }
+}
---
+++ MackHog.Cache.Core/ICache.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace MackHog.Cache.Core
+{
+    public interface ICache
+    {
+        ICacheEntry CreateEntry(string key);
+        IEnumerable<(string Key, object Value)> GetAll();
+        IEnumerable<string> GetKeys();
+        void Remove(string key);
+        bool TryGetValue(string key, out object value);
+    }
+}
